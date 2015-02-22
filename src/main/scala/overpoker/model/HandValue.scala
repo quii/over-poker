aa3770da5214bd671be5605b5515c17ab489e04a
Chris James <qui666@gmail.com>
2015-02-22 16:22:25 +0000

    Refactored fullhouse

--- src/main/scala/overpoker/model/HandValue.scala
+++ src/main/scala/overpoker/model/HandValue.scala
@@ -1,5 +1,7 @@
 package overpoker.model
 
+import scala.collection.immutable.Iterable
+
 sealed trait HandValue
 
 case class Pair(rank: Rank) extends HandValue
@@ -25,19 +27,14 @@ object HandValue{
     val straightFlush = if(straight.isDefined && flush.isDefined) Some(StraightFlush(straight.get.rank)) else None
     val royalFlush = if(straightFlush.isDefined && straightFlush.get.rank==Ace) Some(RoyalFlush) else None
 
-    val cardsWithThreeOfAKindsRemoved = threeOfAKind.map(t=> allCards.filterNot(_.rank==t.rank)).getOrElse(allCards)
 
-    val twoOfAKinds = getTwoOfAKinds(cardsWithThreeOfAKindsRemoved) match{
+    val twoOfAKinds = getTwoOfAKinds(allCards) match{
       case Vector(pair1, pair2) => Some(TwoPair(pair1, pair2))
       case Vector(pair1) => Some(Pair(pair1))
       case Vector() => None
     }
 
-    val fullHouse = (twoOfAKinds, threeOfAKind) match{
-      case (Some(Pair(p)), Some(ThreeOfAKind(t))) => Some(FullHouse(t, p))
-      case (Some(TwoPair(p, _)), Some(ThreeOfAKind(t))) => Some(FullHouse(t, p))
-      case _ => None
-    }
+    val fullHouse = getFullHouse(allCards)
 
     val highCard = Deck(Vector(hand.card1, hand.card2)).sortedByRank
 
@@ -57,6 +54,18 @@ object HandValue{
     .collect { case (rank, Vector(card1, card2, _*)) => rank}
     .toVector
     .sortBy(r => -Rank.toInt(r))
+
+  private def getFullHouse(cards: Vector[Card]):Option[FullHouse] = {
+    val ranked = groupCardsByRank(cards)
+    val cardsInThreeOfAKind = ranked.collect{case (rank, Vector(card1, card2, card3)) => card1}
+
+    cardsInThreeOfAKind.headOption.flatMap{ threeOfAKind =>
+      val threeOfAKindRank = threeOfAKind.rank
+      val cardsWithoutTheThrees = cards.filterNot(x=> x.rank==threeOfAKindRank)
+      val twoOfAKinds = groupCardsByRank(cardsWithoutTheThrees).collect{case (rank, Vector(card1, card2)) => rank}
+      twoOfAKinds.headOption.map(twoRank=> FullHouse(threeOfAKindRank, twoRank))
+    }
+  }
 
   private def getFlush(cards: Vector[Card]) = cards.groupBy(_.suit)
     .collect{case (suit, Vector(card1, card2, card3, card4, card5)) => Vector(card1, card2, card3, card4, card5)}
--- src/test/scala/model/HandValueTest.scala
+++ src/test/scala/model/HandValueTest.scala
@@ -9,12 +9,12 @@ class HandValueTest extends FunSpec{
 
   import TestDeck._
 
-  it("identifies a pair"){
+  it("finds a pair"){
     val pair = Hand(Queen of Clubs, Queen of Diamonds)
     HandValue.getValues(pair, Vector.empty) should contain(Pair(Queen))
   }
 
-  it("identifies a pair from a hand and flop"){
+  it("finds a pair from a hand and flop"){
     val hand = Hand(Queen of Clubs, 2 of Diamonds)
     val flop = Vector(Queen of Diamonds, 10 of Hearts, 8 of Clubs)
     HandValue.getValues(hand, flop) should contain(Pair(Queen))
@@ -25,52 +25,52 @@ class HandValueTest extends FunSpec{
     val flop = Vector(3 of Clubs, 8 of Diamonds, Jack of Clubs)
     HandValue.getValues(hand, flop) should contain(HighCard(5, 2))
   }
-  
-  it("identifies two pairs, ordered by rank"){
+
+  it("finds two pairs, ordered by rank"){
     val flopWithPair = Vector(Queen of Diamonds, Queen of Hearts, 8 of Clubs)
     HandValue.getValues(pairOf2, flopWithPair) should contain(TwoPair(Queen, 2))
   }
 
-  it("identifies three of a kind"){
+  it("finds three of a kind"){
     val flopWithA2 = Vector(Queen of Diamonds, King of Hearts, 2 of Clubs)
     HandValue.getValues(pairOf2, flopWithA2) should contain(ThreeOfAKind(2))
   }
 
-  it("identifies a fullhouse"){
+  it("finds a fullhouse"){
     val flopWithAThreeOfAKind = Vector(Queen of Diamonds, Queen of Hearts, Queen of Clubs)
     HandValue.getValues(pairOf2, flopWithAThreeOfAKind) should contain(FullHouse(Queen, 2))
   }
 
-  it("identifies four of a kind"){
+  it("finds four of a kind"){
     val flopWithTwoTwos = Vector(2 of Diamonds, 2 of Spades, Queen of Spades)
     HandValue.getValues(pairOf2, flopWithTwoTwos) should contain(FourOfAKind(2))
   }
 
-  it("identifies a flush"){
+  it("finds a flush"){
     val flopWithHearts = Vector(4 of Hearts, Queen of Hearts, King of Hearts)
     val heartyHand = Hand(2 of Hearts, 5 of Hearts)
     HandValue.getValues(heartyHand, flopWithHearts) should contain(Flush(King))
   }
 
-  it("identifies a straight"){
+  it("finds a straight"){
     val riverStraight = Vector(2 of Hearts, 7 of Hearts, 6 of Hearts, 8 of Spades, King of Clubs)
     val straightHand = Hand(5 of Hearts, 4 of Hearts)
     HandValue.getValues(straightHand, riverStraight) should contain(Straight(8))
   }
 
-  it("identifies a straight starting with an ace"){
+  it("finds a straight starting with an ace"){
     val flopThatMakesAStraight = Vector(3 of Hearts, 4 of Clubs, 5 of Hearts)
     val straightHand = Hand(Ace of Clubs, 2 of Hearts)
     HandValue.getValues(straightHand, flopThatMakesAStraight) should contain(Straight(5))
   }
 
-  it("identifies a straight flush"){
+  it("finds a straight flush"){
     val flopThatMakesAStraight = Vector(3 of Hearts, 4 of Hearts, 5 of Hearts)
     val straightHand = Hand(Ace of Hearts, 2 of Hearts)
     HandValue.getValues(straightHand, flopThatMakesAStraight) should contain(StraightFlush(5))
   }
 
-  it("identifies a royal flush"){
+  it("finds a royal flush"){
     val flopThatMakesAStraight = Vector(Ace of Hearts, King of Hearts, Queen of Hearts)
     val straightHand = Hand(Jack of Hearts, 10 of Hearts)
     HandValue.getValues(straightHand, flopThatMakesAStraight) should contain(RoyalFlush)
