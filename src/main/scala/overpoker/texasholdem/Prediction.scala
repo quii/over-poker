c1e7698e792e38bb747420c32a5188d90cbfa89f
Chris James <chris.james@springer.com>
2015-03-06 10:37:33 +0000

    Figuring out flush odds on the river

--- src/main/scala/overpoker/texasholdem/Prediction.scala
+++ src/main/scala/overpoker/texasholdem/Prediction.scala
@@ -1,26 +1,47 @@
 package overpoker.texasholdem
 
-import overpoker.playingcards.{Card, Deck, Hand}
+import overpoker.playingcards.{Suit, Card, Deck, Hand}
 
-case class Prediction(value: HandValue, percentageChance: Double)
+import scala.collection.immutable.Iterable
+
+case class Prediction(value: HandValue, percentageChance: Double){
+  override def toString = s"$percentageChance% chance of $value"
+}
 
 object Prediction {
 
   def oddsOfPair(hand: Hand, communityCards: Vector[Card]): Vector[Prediction] = {
-    val allCards = communityCards :+ hand.card1 :+hand.card2
-    val unseenCards = Deck.fullDeck.cards.toSet -- allCards.toSet
-
-    val chanceOfCard = chanceOfCardInDeck(hand.card1, unseenCards)
+    val seenCards = communityCards :+ hand.card1 :+hand.card2
+    val chanceOfMakingPair = chanceOfCardInDeck(hand.card1, unseenCards(seenCards.toSet))
 
     Vector(
-      Prediction(Pair(hand.card1.rank), chanceOfCard),
-      Prediction(Pair(hand.card2.rank), chanceOfCard)
+      Prediction(Pair(hand.card1.rank), chanceOfMakingPair),
+      Prediction(Pair(hand.card2.rank), chanceOfMakingPair)
     )
   }
 
-  def chanceOfCardInDeck(card: Card, cards: Set[Card]): Double ={
-    val outs = cards.count(c=> c.rank==card.rank)
-    (cards.size/outs)
+  //http://poker.stackexchange.com/questions/1/what-are-the-odds-i-will-hit-my-flush
+  def oddsOfFlush(hand: Hand, communityCards: Vector[Card]): Vector[Prediction] = {
+    val seenCards = communityCards :+ hand.card1 :+hand.card2
+
+    val potentialFlush = seenCards.groupBy(c=> c.suit).collect({
+      case (suit, Vector(card1, card2, card3, card4)) => Vector(card1, card2, card3, card4)
+    }).headOption
+
+    potentialFlush.map{ flushySuit =>
+      val outs = 9
+      val chance = probability(outs, unseenCards(seenCards.toSet).size)
+      Vector(Prediction(Flush(flushySuit.sortBy(x=> -x.rank.toInt).head.rank), chance))
+    }.getOrElse(Vector.empty)
   }
+
+  private def chanceOfCardInDeck(card: Card, cards: Set[Card]): Double ={
+    val outs = cards.count(c=> c.rank==card.rank)
+    probability(outs, cards.size)
+  }
+
+  private def probability(outs: Int, totalCards: Int) = "%.1f".format((outs.toDouble / totalCards.toDouble) * 100).toDouble
+
+  private def unseenCards(seen: Set[Card]) = Deck.fullDeck.cards.toSet -- seen
 
 }
--- src/test/scala/model/texasholdem/PredictionTest.scala
+++ src/test/scala/model/texasholdem/PredictionTest.scala
@@ -11,10 +11,24 @@ import overpoker.texasholdem.Prediction._
 class PredictionTest extends FunSpec{
 
   val uselessHand = Hand(2 of Clubs, 3 of Spades)
+  val flushingHand = Hand(2 of Clubs, 3 of Clubs)
 
-  it("figures out the odds of the river making a pair"){
-    val turn = Vector(10 of Spades, King of Clubs, 5 of Diamonds, 6 of Clubs)
-    oddsOfPair(uselessHand, turn) should (contain(Prediction(Pair(2), 15)) and contain(Prediction(Pair(3), 15)))
+  describe("the river"){
+
+    it("figures out the odds of making a pair"){
+      val turn = Vector(10 of Spades, King of Clubs, 5 of Diamonds, 6 of Clubs)
+      oddsOfPair(uselessHand, turn) should (contain(Prediction(Pair(2), 6.5)) and contain(Prediction(Pair(3), 6.5)))
+    }
+
+    it("knows you cant make a flush"){
+      val turn = Vector(2 of Hearts, 10 of Spades, 6 of Diamonds, King of Clubs)
+      oddsOfFlush(uselessHand, turn) should be('empty)
+    }
+
+    it("figures out the odds of you making a flush"){
+      val turn = Vector(10 of Clubs, 9 of Clubs, Ace of Hearts, Queen of Diamonds)
+      oddsOfFlush(flushingHand, turn) should contain(Prediction(Flush(10), 19.6))
+    }
+
   }
-
 }
