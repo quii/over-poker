eb4b4403a2b4f3e33660db6d16784f3dc6082d71
David Wickes <dave.wickes@gmail.com>
2021-05-10 16:05:19 +0100

    s - production code compiling - test code, not so much

--- src/main/scala/overpoker/web/Controller.scala
+++ src/main/scala/overpoker/web/Controller.scala
@@ -1,13 +1,12 @@
 package overpoker.web
 
 import cats.effect.IO
-import io.circe.Json.JString
-import io.circe.generic.semiauto
+import io.circe.generic.semiauto.deriveDecoder
 import io.circe.{Decoder, Json}
 import org.http4s.HttpRoutes
 import org.http4s.circe.CirceEntityCodec.{circeEntityDecoder, circeEntityEncoder}
 import org.http4s.dsl.io._
-import overpoker.playingcards.{Card, PlayerHand}
+import overpoker.playingcards.{Card, PlayerHand, Rank, Suit}
 import overpoker.texasholdem.hands.Hand
 
 trait HandIdentityService {
@@ -22,13 +21,16 @@ object Controller {
 
   import parsing.PlayingCardCodecs._
 
-  implicit def decoder: Decoder[HandRequest] = semiauto.deriveDecoder[HandRequest]
+  implicit val decoder: Decoder[HandRequest] = deriveDecoder[HandRequest]
+  implicit val cardDecoder: Decoder[Card] = deriveDecoder[Card]
+  implicit val rankDecoder: Decoder[Rank] = deriveDecoder[Rank]
+  implicit val suitDecoder: Decoder[Suit] = deriveDecoder[Suit]
 
   def getHand(service: HandIdentityService) = HttpRoutes.of[IO] {
     case request@POST -> Root / "hand" =>
       for {
         cards <- request.as[HandRequest]
-        json = Json.obj(("hand", JString(service.identifyHand(PlayerHand(cards.player(0), cards.player(1)), cards.community).toString)))
+        json = Json.obj(("hand", Json.fromString(service.identifyHand(PlayerHand(cards.player(0), cards.player(1)), cards.community).toString)))
         resp <- Ok(json)
       } yield resp
   }
